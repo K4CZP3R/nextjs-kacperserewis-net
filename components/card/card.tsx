4756a7ad6bcc5fe59d97756d8ddd0de4491c201d
Kacper Serewiś <kacper@kacperserewis.net>
2022-11-09 17:20:01 +0100

    fix buttons

--- components/card/card.tsx
+++ components/card/card.tsx
@@ -1,13 +1,13 @@
 import React from "react";
+import { StringKeyframeTrack } from "three";
 import Button from "../button/button";
 import styles from "./card.module.css";
 
 export type CardProps = JSX.IntrinsicElements["div"] & {
   title: string;
   description: string;
-  link: string;
-  linkText: string;
-  subTitle: string;
+  hashTags?: string[];
+  subTitle?: string;
 };
 
 export default class Card extends React.Component<CardProps> {
@@ -16,7 +16,16 @@ export default class Card extends React.Component<CardProps> {
       <div className={styles.card}>
         <div className={styles.card_body}>
           <h5 className={styles.card_title}>{this.props.title}</h5>
-          <span className={styles.card_subtitle}>{this.props.subTitle}</span>
+          {!this.props.subTitle ?? (
+            <>
+              <span className={styles.card_subtitle}>
+                {this.props.subTitle}
+              </span>
+              <br />
+            </>
+          )}
+
+          <span>{(this.props.hashTags ?? []).join(" ")}</span>
           <p className={styles.card_text}>{this.props.description}</p>
           {this.props.children}
         </div>
--- models/post.model.ts
+++ models/post.model.ts
@@ -5,32 +5,37 @@ attributes {
   description
   content
   slug
+  createdAt
+  updatedAt
   tags {
     value
   }
 }
-`
 
+`;
 
 export interface IPost {
-    title: string;
-    id: string;
-    description: string;
-    content: string;
-    tags: {value:string}[];
-    images: any;
-    slug: string;
+  title: string;
+  id: string;
+  description: string;
+  content: string;
+  tags: { value: string }[];
+  images: any;
+  slug: string;
+  createdAt: string;
+  updatedAt: string;
 }
 
-
 export function toPost(raw: any): IPost {
-    return {
-        id: raw.id,
-        title: raw.attributes.title,
-        description: raw.attributes.description,
-        content: raw.attributes.content,
-        tags: raw.attributes.tags,
-        images: raw.attributes.images ?? null,
-        slug: raw.attributes.slug
-    }
+  return {
+    id: raw.id,
+    title: raw.attributes.title,
+    description: raw.attributes.description,
+    content: raw.attributes.content,
+    tags: raw.attributes.tags,
+    images: raw.attributes.images ?? null,
+    slug: raw.attributes.slug,
+    createdAt: raw.attributes.createdAt,
+    updatedAt: raw.attributes.updatedAt,
+  };
 }
