db7fd8a16e55dc40ed8b902c08ea49dd14ce5e18
Kacper Serewiś <kacper@kacperserewis.net>
2023-01-09 23:00:21 +0100

    Update projects.ts

--- data/projects.ts
+++ data/projects.ts
@@ -8,17 +8,9 @@ function githubLink(user: string, repository: string) {
 
 export const PROJECTS: IProject[] = [
     {
-        title: "First LoveBox - ploveu",
-        slug: "lovebox-1",
-        description: "An (failed) attempt to create a Lovebox device, which allows to send messages to your loved ones. The device was based on ESP32 with E-Ink display and used 3D printed case",
-        buttons: [],
-        tags: ["ESP32", "C++"]
-
-    },
-    {
         title: "LoRa Trackers",
         slug: "lora-trackers",
-        description: 'OpenRemote has asked us to connect a "wacky" IoT device to their IoT platform. After some brainstorming we came up with our concept: a GPS tracker which works via long-range radio (a.k.a. LoRa) technology. The GPS trackers communicate over LoRa in a mesh network and send their locations via an edge device to the cloud-hosted OpenRemote platform. While a "wacky" use-case for LoRa technology, we were able to make the system function as designed. Using LoRa technology for the GPS tracker mesh network helps devices stay connected even at long ranges, while being battery efficient.',
+        description: 'OpenRemote requested we connect a quirky IoT device to their platform. We developed a GPS tracker using LoRa technology for communication. The tracker\'s mesh network sends location data through an edge device to the cloud-based OpenRemote platform. The LoRa-based mesh network enables long-range connectivity and low battery usage for the devices.',
         buttons: [{
             title: "View Source",
             path: githubLink("openremote", "or-loratrackers")
@@ -27,12 +19,12 @@ export const PROJECTS: IProject[] = [
             title: "View Demo",
             path: "https://www.youtube.com/watch?v=72r54r_dwo8"
         }],
-        tags: ["LoRa", "IoT", "OpenRemote", "GPS", "Mesh Network"],
+        tags: ["#LoRa", "#IoT", "#OpenRemote", "#GPS", "#MeshNetwork"],
     },
     {
         title: "FaceNotify / TrustNotify",
         slug: "facenotify",
-        description: "An app that makes it possible to show the notifications on the lock screen only if the device owner is looking at it. The first version was written in Java, the second one, more optimized, in Kotlin.",
+        description: "I've developed an app that allows notifications to be displayed on the lock screen only when the device owner is looking at it. The first version was written in Java, while the second, more optimized version was written in Kotlin.",
         buttons: [
             {
                 title: "View Source #1",
@@ -47,22 +39,22 @@ export const PROJECTS: IProject[] = [
                 path: "https://youtu.be/Gz_cFyi5riw?t=67"
             }
         ],
-        tags: ["Android", "Java", "Kotlin", "Face Recognition", "Notification"],
+        tags: ["#Android", "#Java", "#Kotlin", "#FaceRecognition", "#Notification"],
     },
     {
         title: "P2PLauncher",
         slug: "p2plauncher",
-        description: "GUI for a program (freelan) that makes it possible to connect peer-to-peer between multiple computers, allowing them to for ex. play old LAN-only games.",
+        description: "P2PLauncher is a program that helps connect multiple peers in a simulated LAN environment using FreeLAN, an open-source, peer-to-peer VPN. It has a user-friendly interface and is mainly intended for gamers. P2PLauncher enables users to play old games with LAN connectivity, even if the game's servers are no longer supported or have networking issues with their current systems.",
         buttons: [{
             title: "View Source",
             path: githubLink("K4CZP3R", "P2PLauncher")
         }],
-        tags: ["C#", "P2P", "LAN", "Freelan"],
+        tags: ["#csharp", "#P2P", "#LAN", "#Freelan"],
     },
     {
         title: "Reverse engineering of smart plug (TP-Link Tapo)",
         slug: "tapo",
-        description: "I've reverse engineered the protocol used for communication between the Tapo app and the smart plug itself. There are some implementations of my research available on GitHub, ex: Fishbigger's TapoP100 repository.",
+        description: "I have successfully reverse-engineered the protocol used for communication between the Tapo app and the smart plug. My research and its implementations can be found on GitHub, such as in the Fishbigger's TapoP100 repository.",
         buttons: [{
             title: "View Source #1",
             path: githubLink("K4CZP3R", "tapo-p100-python")
@@ -76,29 +68,29 @@ export const PROJECTS: IProject[] = [
             path: "/blog/post/reverse-engineering-tp-link-tapo"
         }
     ],
-        tags: ["Python", "Reverse Engineering", "Smart Plug", "TP-Link", "Tapo"],
+        tags: ["#Python", "#ReverseEngineering", "#SmartPlug", "#TP-Link", "#Tapo"],
     },
     {
         title: "HyperX keyboard USB protocol implementation",
         slug: "hyperx",
-        description: "I've created simple proof-of-concept which allows to control every LED on the keyboard. This proof-of-concept was used to add support for this keyboard in OpenRGB app.",
+        description: "I have developed a proof-of-concept that allows for full control of the LEDs on a HyperX keyboard. This proof-of-concept was utilized to add support for this keyboard in the OpenRGB app.",
         buttons: [{
             title: "View Source",
             path: githubLink("K4CZP3R", "hpyerx-keyboard-rgb")
         }],
-        tags: ["C++", "USB", "HyperX", "OpenRGB"],
+        tags: ["#C++", "#USB", "#HyperX", "#OpenRGB"],
     },
     {
         title: "LoveBox - ploveu",
         slug: "lovebox",
-        description: "My own do-it-yourself Lovebox device, which allows to send messages to your loved ones. The device is based on Raspberry Pi and uses 3D printed case",
+        description: "I recently created a special project called \"PLU-box\" as a gift for my girlfriend on our 5th anniversary. It's a device that allows us to send and receive heartfelt messages from each other, no matter how far apart we are.",
         buttons: [
         //     {
         //     title: "Blog post",
         //     path: "/blog/lovebox-pt1"
         // }
         ],
-        tags: ["TypeScript", "Linux", "Docker"]
+        tags: ["#TypeScript", "#Linux", "#Docker", "#MongoDB", "#Lit", "#WebComponents"]
     }
 
 ]
