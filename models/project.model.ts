55cbcba0f96099c8627ca26e4149202ed60ce4e4
Kacper Serewiś <kacper@stofloos.nl>
2024-02-10 21:18:09 +0100

    Add BadgeConfigSchema and getBadgeConfigKey functions to project.model.ts

--- models/project.model.ts
+++ models/project.model.ts
@@ -1,4 +1,31 @@
 import { Locale } from "@/locales/consts";
+import { z } from "zod";
+
+export const BadgeConfigSchema = z.object({
+  type: z.enum(["languages", "total_time"]),
+  source: z.enum(["wakapi", "static"]),
+  project: z.string().optional(),
+  label: z.string().optional(),
+  value: z.string().optional(),
+});
+
+export type BadgeConfig = z.infer<typeof BadgeConfigSchema>;
+
+export const getBadgeConfigKey = ({
+  label,
+  project,
+}: {
+  label?: string;
+  project?: string;
+}) => {
+  if (project) {
+    return `project-${project}`;
+  } else if (label) {
+    return `label-${label}`;
+  } else {
+    throw new Error("Invalid badge config");
+  }
+};
 
 export interface IProject {
   title: string;
@@ -6,5 +33,6 @@ export interface IProject {
   description: string;
   buttons: { title: string; path: string }[];
   tags: string[];
+  badges: BadgeConfig[] | null;
   locale: Locale;
 }
