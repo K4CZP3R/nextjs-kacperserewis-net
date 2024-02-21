a4ebab7184dbeb09cce9b82d3c2359e7103d8997
Kacper Serewiś <kacper@stofloos.nl>
2024-02-22 00:09:33 +0100

    move from next-international to next-intl

--- locales/pl.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  index: "Index",
-  projects: "Projekty",
-  blogPage: "Blog",
-  projectsPageDescription: "Oto kilka moich projektów",
-  blogPageDescription: "Oto kilka moich wpisów na blogu",
-  loading: "Ładowanie...",
-  wait: "Chwila...",
-} as const;
--- locales/server.ts
+++
@@ -1,8 +0,0 @@
-import { createI18nServer } from "next-international/server";
-
-export const { getI18n, getScopedI18n, getStaticParams, getCurrentLocale } =
-  createI18nServer({
-    en: () => import("./en"),
-    pl: () => import("./pl"),
-    nl: () => import("./nl"),
-  });
